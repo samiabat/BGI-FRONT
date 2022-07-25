edbe21ee027335ccec7f018ab858165b4f51b216
samiabat <samuelabatneh20@gmail.com>
2022-07-26 00:48:29 +0300

    add changes

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { ConfirmDeleteModule } from '../confirm-delete/confirm-delete.module';
+import { SectorsModule } from '../sectors/sectors.module';
+import { RolesModule } from '../roles/roles.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    ConfirmDeleteModule,
+    SectorsModule,
+    RolesModule,
   ]
 })
 export class DashboardModule { }
