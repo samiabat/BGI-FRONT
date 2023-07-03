6537dbe77a7b575f83100932b6098be58ce728e9
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2023-07-03 23:25:33 +0300

    Update urls.ts

--- src/app/constants/urls.ts
+++ src/app/constants/urls.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:8000';
+export const API_BASE_URL = 'https://accountmanager.onrender.com';
 
 // URLs
 export const ROLES_URL = `${API_BASE_URL}/roles/`;
