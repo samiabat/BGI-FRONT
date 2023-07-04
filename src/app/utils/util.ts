f20689435013fd0fed3c3dbdb1d575fc1732c3f6
unknown <samuelabatneh20@gmail.com>
2023-07-04 21:47:00 +0300

    add

--- src/app/utils/util.ts
+++ src/app/utils/util.ts
@@ -1,16 +1,16 @@
-import { AbstractControl } from '@angular/forms';
-import { ENGLISH } from '../constants/language';
+// import { AbstractControl } from '@angular/forms';
+// import { ENGLISH } from '../constants/language';
 
-export const addInputMask = (
-  control: AbstractControl | null,
-  cultureName: String
-) => {
-  const pattern =
-    cultureName === ENGLISH ? /[^a-zA-Z 0-9]*/gi : /[^\u1200-\u137F 0-9]*/gi;
-  control?.valueChanges.subscribe((change) => {
-    console.log(change);
+// export const addInputMask = (
+//   control: AbstractControl | null,
+//   cultureName: String
+// ) => {
+//   const pattern =
+//     cultureName === ENGLISH ? /[^a-zA-Z 0-9]*/gi : /[^\u1200-\u137F 0-9]*/gi;
+//   control?.valueChanges.subscribe((change) => {
+//     console.log(change);
     
 
-    control?.setValue(change.replace(pattern, ''), { emitEvent: false });
-  })
-};
+//     control?.setValue(change.replace(pattern, ''), { emitEvent: false });
+//   })
+// };
